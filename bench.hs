c5a14a0f1866e68a08837ac679bd71700273c41e
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2012-11-29 16:05:52 +0900

    add benchmarks of generic-deriving package's gshow

--- bench.hs
+++ bench.hs
@@ -5,18 +5,24 @@ import qualified Data.ByteString.Char8      as S
 import qualified Data.ByteString.Lazy.Char8 as L
 import           GHC.Generics
 
+import           Generics.Deriving.Show
+
 import           Blaze.Show
 
 data Foo a = Foo { fooA :: a, fooB :: Bool } deriving (Show, Generic)
 instance BlazeShow a => BlazeShow (Foo a)
+instance GShow a => GShow (Foo a)
 
-benchAll :: (Show a, BlazeShow a) => a -> [Benchmark]
+benchAll :: (Show a, GShow a, BlazeShow a) => a -> [Benchmark]
 benchAll x =
-  [ bench "show"        $ nf show    x
-  , bench "S.pack.show" $ nf (S.pack . show) x
-  , bench "L.pack.show" $ nf (L.pack . show) x
-  , bench "showBS"      $ nf showBS  x
-  , bench "showLBS"     $ nf showLBS x
+  [ bench "show"         $ nf show    x
+  , bench "S.pack.show"  $ nf (S.pack . show) x
+  , bench "L.pack.show"  $ nf (L.pack . show) x
+  , bench "gshow"        $ nf gshow    x
+  , bench "S.pack.gshow" $ nf (S.pack . gshow) x
+  , bench "L.pack.gshow" $ nf (L.pack . gshow) x
+  , bench "showBS"       $ nf showBS  x
+  , bench "showLBS"      $ nf showLBS x
   ]
 
 main :: IO ()
